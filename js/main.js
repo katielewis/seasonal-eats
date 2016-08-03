ed4675d43088d78c3b912731b0a64c297cc0f85e
katielewis <katie@codeforamerica.org>
2016-08-03 00:15:21 -0700

    fall section + half of winter section

--- js/main.js
+++ js/main.js
@@ -1,3 +1,4 @@
+
 //spring section
 var springOffset = $('#spring').offset().top;
 
@@ -25,5 +26,25 @@ var summerOffset = $('#summer').offset().top;
 $(window).on('scroll', function () {
 	if ($(window).scrollTop() >= (summerOffset / 1.5)) {
 		$('#summer-text-box').addClass('text-box-fade');
+	}
+});
+
+
+//fall section
+var fallOffset = $('#fall').offset().top;
+
+$(window).on('scroll', function () {
+	if ($(window).scrollTop() >= (fallOffset / 1.2)) {
+		$('#fall-text-box').addClass('text-box-fade');
+	}
+});
+
+
+//winter section
+var winterOffset = $('#winter').offset().top;
+
+$(window).on('scroll', function () {
+	if ($(window).scrollTop() >= (winterOffset / 1.2)) {
+		$('#winter-text-box').addClass('text-box-fade');
 	}
 });
