17f065a3db6f9392f7e73bfe009c7d5b03dd4c00
katielewis <katie@codeforamerica.org>
2016-08-03 20:33:42 -0700

    added all winter section
    added things to winter, added some bounce to summer, stuck with the
    last two animations

--- js/main.js
+++ js/main.js
@@ -7,10 +7,12 @@ $(window).on('scroll', function () {
 	if ($(window).scrollTop() >= (springOffset - 20)) {
 		$('#lemonSlice').addClass('lemonSliceAnimate');
 		$('#orange').addClass('orangeAnimate');
-	} else {
-		$('#lemonSlice').removeClass('lemonSliceAnimate');
-		$('#orange').removeClass('orangeAnimate');
-	}
+	} 
+	// none of the other animations so on scrolling up, so hiding
+	// else {
+	// 	$('#lemonSlice').removeClass('lemonSliceAnimate');
+	// 	$('#orange').removeClass('orangeAnimate');
+	// }
 });
 
 $(window).on('scroll', function () {
@@ -29,6 +31,14 @@ $(window).on('scroll', function () {
 	}
 });
 
+$(window).on('scroll', function () {
+	if ($(window).scrollTop() >= (summerOffset - 40)) {
+		$('#nectarine ,#plum ,#peach').addClass('fruitBounce');
+		$('#strawberry').addClass('strawberryBounce');
+		$('#cherries ,#figs').addClass('secondBounce');
+	}
+});
+
 
 //fall section
 var fallOffset = $('#fall').offset().top;
@@ -39,6 +49,13 @@ $(window).on('scroll', function () {
 	}
 });
 
+$(window).on('scroll', function () {
+	if ($(window).scrollTop() >= (fallOffset - 30)) {
+		$('#squash').addClass('gordsMove');
+	}
+});
+
+
 
 //winter section
 var winterOffset = $('#winter').offset().top;
@@ -47,4 +64,10 @@ $(window).on('scroll', function () {
 	if ($(window).scrollTop() >= (winterOffset / 1.2)) {
 		$('#winter-text-box').addClass('text-box-fade');
 	}
+});
+
+$(window).on('scroll', function () {
+	if ($(window).scrollTop() >= (winterOffset - 20)) {
+		$('#kale').addClass('veggieFan');
+	} 
 });
